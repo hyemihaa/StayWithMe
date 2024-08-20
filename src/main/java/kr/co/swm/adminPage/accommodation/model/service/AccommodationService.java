04725137026c3e5564fe9fd11cc17117f0b47bb6
jinwoo <dlwlsdn8022@gmail.com>
2024-08-20 20:09:54 +0900

    feat : 객실 별 개수만큼 객식 추가 및 객실에 대한 가격 인입

--- src/main/java/kr/co/swm/adminPage/accommodation/model/service/AccommodationService.java
+++ src/main/java/kr/co/swm/adminPage/accommodation/model/service/AccommodationService.java
@@ -6,6 +6,6 @@ import kr.co.swm.adminPage.accommodation.model.dto.AccommodationImageDto;
 public interface AccommodationService {
 
     int saveAccommodation(AccommodationDto accommodationDto, AccommodationImageDto mainImage);
-    int enrollRooms(AccommodationDto  accommodationDto, String roomCategory, String roomName, String checkIn, String checkOut);
-    void enrollSubImages(AccommodationImageDto roomImages, int roomNo);
+    int enrollRooms(AccommodationDto  accommodationDto, String roomCategory, String roomName, String checkIn, String checkOut, int roomValue, AccommodationDto roomRate);
+    int enrollSubImages(AccommodationImageDto roomImages, AccommodationDto accommodationDto);
 }
