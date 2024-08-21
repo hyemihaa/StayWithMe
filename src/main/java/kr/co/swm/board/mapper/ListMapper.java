c7e9bd6e2b08a8155079eb8b4f17bae3abe2e971
sanghyeon <ysh940129@gmail.com>
2024-08-21 15:24:52 +0900

    feat: filter search

--- src/main/java/kr/co/swm/board/mapper/ListMapper.java
+++ src/main/java/kr/co/swm/board/mapper/ListMapper.java
@@ -2,6 +2,7 @@ package kr.co.swm.board.mapper;
 
 import kr.co.swm.board.list.model.DTO.ListDTO;
 import kr.co.swm.board.list.model.DTO.PageInfoDTO;
+import kr.co.swm.board.list.model.DTO.SearchDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,11 +11,13 @@ import java.util.List;
 @Mapper
 public interface ListMapper {
   //  게시글 불러오기
-  List<ListDTO> getPlace(@Param("pi") PageInfoDTO pi);
+//  List<ListDTO> getPlace(@Param("pi") PageInfoDTO pi);
+  List<ListDTO> getPlace(@Param("pi") PageInfoDTO pi, @Param("searchDTO") SearchDTO searchDTO);
   // 총 게시글 개수
-  int getTotalCount();
+  int getTotalCount(@Param("searchDTO") SearchDTO searchDTO);
   // 별점 평균
   double getAvgRate(@Param("boardNo") int boardNo);
   // 기본 가격
   List<ListDTO> getCost();
+
 }
