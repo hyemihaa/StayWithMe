2b88b4ecb431f6214fc49e8bc8d144d3a6311e5e
jinwoo <dlwlsdn8022@gmail.com>
2024-08-25 13:35:16 +0900

    feat : 쿠폰 등록시 쿠폰정보 테이블에 재고수량과 사용량 업데이트
    feat : 쿠폰 모두 받기 클릭시 해당 쿠폰 리스트 모두 인입

--- src/main/java/kr/co/swm/coupon/model/service/AddCouponServiceImpl.java
+++ src/main/java/kr/co/swm/coupon/model/service/AddCouponServiceImpl.java
@@ -2,6 +2,7 @@ package kr.co.swm.coupon.model.service;
 
 import kr.co.swm.coupon.mapper.AddCouponMapper;
 import kr.co.swm.model.dto.WebDto;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +20,8 @@ public class AddCouponServiceImpl implements AddCouponService {
 
 
     @Override
-    public List<WebDto> couponList() {
-        List<WebDto> list = addCouponMapper.couponList();
+    public List<WebDto> couponList(Long userId) {
+        List<WebDto> list = addCouponMapper.couponList(userId);
         return list;
     }
 
@@ -30,5 +31,13 @@ public class AddCouponServiceImpl implements AddCouponService {
         return addCouponMapper.addCoupon(couponId, userId);
     }
 
+    @Override
+    public int updateCouponQuantity(int couponId) {
+        return addCouponMapper.updateCouponQuantity(couponId);
+    }
+
+    public int addAllCoupons(int couponIds, Long userNo){
+        return addCouponMapper.addAllCoupons(couponIds, userNo);
+    }
 
 }
